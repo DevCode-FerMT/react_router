869b565e86754eba94d209cd94e0611bd6ff4fee
FerMT <v.fh16.9542.20XX@gmail.com>
2023-09-28 14:20:20 -0600

    navegation SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,56 @@
-function App() {
-  return <h1>Routes</h1>;
+import { useEffect, useState } from 'react'
+
+const NAV_EVNT = 'pushState'
+
+function navigate (href) {
+  window.history.pushState({}, '', href)
+  // crear un evento personalizado para avisar que se ha cambiado la URL
+  const navigationEvent = new Event(NAV_EVNT)
+  // enviar el evento para que se pueda escuchar
+  window.dispatchEvent(navigationEvent)
 }
 
-export default App;
+function HomePage () {
+  return (
+    <>
+      <h1>Home</h1>
+      <button onClick={() => navigate('/about')}>About</button>
+    </>
+  )
+}
+
+function AboutPage () {
+  return (
+    <>
+      <h1>About</h1>
+      <button onClick={() => navigate('/')}>Home</button>
+    </>
+  )
+}
+
+function App () {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname)
+    }
+
+    window.addEventListener(NAV_EVNT, onLocationChange)
+    window.addEventListener('popstate', onLocationChange)
+
+    return () => {
+      window.removeEventListener(NAV_EVNT, onLocationChange)
+      window.removeEventListener('popstate', onLocationChange)
+    }
+  }, [])
+
+  return (
+    <>
+      {currentPath === '/' && <HomePage />}
+      {currentPath === '/about' && <AboutPage />}
+    </>
+  )
+}
+
+export default App
